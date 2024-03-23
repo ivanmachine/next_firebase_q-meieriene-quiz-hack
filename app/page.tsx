f504b912da451579522b237196189bd14928cecb
Ivan Tarnyagin <ivan.tarnyagin@gmail.com>
2024-03-23 01:04:46 +0100

    Added components and lib to mv, and started hacking the quiz itself

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import Leaderboard from "@/components/Leaderboard";
 import s from "./page.module.scss";
+import Hackquiz from "@/components/Hackquiz";
 
 export default function Home() {
   return (
     <main className={`${s.main} container`}>
       <h1 className={s.header}>Homepage</h1>
       <section className={s.content}>
-        <div>Other stuf</div>
+        <Hackquiz />
         <Leaderboard />
       </section>
     </main>
