84c0faccabc89689b583f07e301b73223bef3b6c
Ivan Tarnyagin <ivan.tarnyagin@gmail.com>
2024-03-25 14:54:45 +0100

    Now also copying over middleware

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,23 @@
 export default function page() {
   return (
     <main className={`container`}>
-      <h1>About page</h1>
+      <h1>Om</h1>
+      <h2>Hva er dette?</h2>
+      <p>
+        Dette er en nettside som jeg brukte til å få perfekt score på
+        q-meieriene sin påskequiz.
+      </p>
+      <h2>Hvorfor?</h2>
+      <p>
+        Jeg er interesert i nettverk, og sikkerhet, og ville løse en gøy oppgave
+      </p>
+      <h2>Hvordan?</h2>
+      <p>Prosessen er delt i tre steg</p>
+      <ol>
+        <li>Jeg trener databasen</li>
+        <li>Jeg fullfører quizzen</li>
+        <li>Jeg registrerer svaret</li>
+      </ol>
     </main>
   );
 }
